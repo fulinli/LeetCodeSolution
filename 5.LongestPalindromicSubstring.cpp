46f0d574ae9eff31e7a45ab37eca17a62c87fbc3
Flynn <812264301@qq.com>
2018-07-27 17:13:38 +0800

    add dynamic programming solution, but not pass.

--- 5.LongestPalindromicSubstring.cpp
+++ 5.LongestPalindromicSubstring.cpp
@@ -11,7 +11,8 @@ Example 2:
 Input: "cbbd"
 Output: "bb" 
 */
-class Solution
+//Solution1为暴力法，超出时间限制。
+class Solution1
 {
   public:
     string longestPalindrome(string s)
@@ -55,3 +56,49 @@ class Solution
     }
 };
 //"dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" 测试超过时间限制
+//Solution2 通过学习，使用了动态规划方法，写的混乱，未通过。   输入
+class Solution2 {
+public:
+    string longestPalindrome(string s) 
+    {
+        string str;
+        int maxlength = 1;
+        int strlength = s.length();
+        int dp[1001][1001];
+        for(int i = 0; i < strlength; i++)
+        {
+            dp[i][i] = 1;
+            if(i<=strlength-1)
+            {
+                
+                if(maxlength<=1)
+                    str = s.substr(i, 1);
+                if(s[i+1] == s[i])
+                { 
+                    dp[i][i+1] = 1;
+                    maxlength = 2;
+                    str = s.substr(i, 2);
+
+                }
+            }
+        }
+        for(int L = 3; L <= strlength; L++)
+        {
+            cout<<"L ="<<L<<endl;
+            for(int j = 0; j < strlength - L + 1; j++)
+            {
+                if((s[j] == s[L+j-1]) && dp[j+1][L+j-2]==1)
+                {
+                    dp[j][L+j-1] = 1;
+                    if(L>=maxlength)
+                    {
+                        cout<<"str="<<str<<endl;
+                        str = s.substr(j, L);
+                        maxlength = L;
+                    }
+                }
+            }
+        }
+        return str;
+    }
+};
