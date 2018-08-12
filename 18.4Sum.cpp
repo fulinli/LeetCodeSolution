0628e9e10946b8746dc904d333ce41b40fb6c017
Flynn <812264301@qq.com>
2018-08-12 17:05:12 +0800

    unfinished

--- 18.4Sum.cpp
+++ 18.4Sum.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> res;
+        vector<vector<int> > res;
         int val;
-        vector<int> t;
+        vector<int> t(4);
         int n = nums.size();
         if(n < 4){
-            return vector<vector<int>>;
+            return res;
         }
         sort(nums.begin(), nums.end());
         for(int i = 0; i < n - 3; i++){
             for(int j = i + 1; j < n - 2; j++){
                 for(int k = j + 1; k < n - 1; k++){
-                    for(int l = k + 1; l < n; l++){
+                    int l = k + 1;
+                    while(l < n)
+                    {
                         val = nums[i] + nums[j] + nums[k] + nums[l];
                         if(nums[l]==nums[l+1]){
                             l++;
                             continue;
                         }
-                        if(nums[k]==nums[k+1]){
+                        if(nums[k]==nums[k+1] && k+1 != l){
                             k++;
                             continue;
                         }
-                        if(nums[j]==nums[j+1]){
+                        if(nums[j]==nums[j+1] && j+1 != k){
                             j++;
                             continue;
                         }
-                        if(nums[i]==nums[i+1]){
+                        if(nums[i]==nums[i+1] && i+1 != j){
                             i++;
                             continue;
                         }
                         if(val == target){
-                            t.push_back(nums[i]);
-                            t.push_back(nums[j]);
-                            t.push_back(nums[k]);
-                            t.push_back(nums[l]);
+                            t[0] = nums[i];
+                            t[1] = nums[j];
+                            t[2] = nums[k];
+                            t[3] = nums[l];
+                            cout<<"t = "<<t[0]<<t[1]<<t[2]<<t[3]<<endl;
                             res.push_back(t);
                         }
-                        t.clear();
+                        l++;
                     }
                 }
             }
