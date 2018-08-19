a89713a1a22b59acf4f9470c42e5f322ef8f597d
Flynn <812264301@qq.com>
2018-08-19 18:53:22 +0800

    done!

--- 18.4Sum.cpp
+++ 18.4Sum.cpp
@@ -1,38 +1,50 @@
+// debug 屡次失败，最后发现是由于在判断nums[l]==nums[l+1]时越界，加上l+1!=n这条限制后通过
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int> > res;
+        vector<vector<int>> res;
+        int a;
         int val;
         vector<int> t(4);
         int n = nums.size();
+        a = target;
         if(n < 4){
             return res;
         }
         sort(nums.begin(), nums.end());
         for(int i = 0; i < n - 3; i++){
+            //cout<<"i = "<<i<<endl;
             for(int j = i + 1; j < n - 2; j++){
+                //cout<<"j = "<<j<<endl;
                 for(int k = j + 1; k < n - 1; k++){
+                    //cout<<"k = "<<k<<endl;
                     int l = k + 1;
+                    //cout<<"l = "<<l<<endl;
                     while(l < n)
                     {
+                        
                         val = nums[i] + nums[j] + nums[k] + nums[l];
-                        if(nums[l]==nums[l+1]){
+                        if(l+1 != n && nums[l]==nums[l+1]){
                             l++;
+                        
                             continue;
                         }
                         if(nums[k]==nums[k+1] && k+1 != l){
                             k++;
+                           
                             continue;
                         }
                         if(nums[j]==nums[j+1] && j+1 != k){
                             j++;
+                          
                             continue;
                         }
                         if(nums[i]==nums[i+1] && i+1 != j){
                             i++;
+                       
                             continue;
                         }
-                        if(val == target){
+                        if(val == a){
                             t[0] = nums[i];
                             t[1] = nums[j];
                             t[2] = nums[k];
