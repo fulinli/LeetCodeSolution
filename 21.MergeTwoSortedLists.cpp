aa6b5d6c62501db2360a83af215a652ab1127b1e
Flynn <812264301@qq.com>
2018-11-26 23:30:02 +0800

    done!

---
+++ 21.MergeTwoSortedLists.cpp
@@ -0,0 +1,110 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        struct ListNode *p = l1;
+        struct ListNode *q = l2;
+        struct ListNode *s, *t;
+        s = l1->val >= l2->val ? l2 : l1;
+        t = s;
+        while(p->next != NULL || q->next != NULL){
+            if(p->next!=NULL && q->next != NULL){
+                if(p->val < q->val){
+                    t->next = p;
+                    p = p->next;
+                    t = t->next;
+                }
+                else if(p->val > q->val){
+                    t->next = q;
+                    q = q->next;
+                    t = t->next;
+                }
+                else{
+                    t->next = p;
+                    t = t->next;
+                    t->next = q;
+                    p = p->next;
+                    q = q->next;
+                }
+            }
+            if(p->next == NULL && q->next != NULL){
+                t->next = q;
+            }
+            if(p->next != NULL && q->next == NULL){
+                t->next = p;
+            }
+        }
+        return s;
+    }
+};  
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1==NULL && l2 == NULL){
+            return l1;
+        }else if(l1==NULL && l2 != NULL){
+            return l2;
+        }else if(l1!=NULL && l2 == NULL){
+            return l1;
+        }else{
+            struct ListNode *p = l1;
+            struct ListNode *q = l2;
+            struct ListNode *s, *t;
+            if(p->val >= q->val){
+                s = new ListNode(q->val);
+                q = q->next;
+            }
+            else{
+                s = new ListNode(p->val);
+                p = p->next;
+            }
+            t = s;
+            while(q != NULL && p != NULL){
+                if(p->val < q->val){
+                    t->next = new ListNode(p->val);
+                    p = p->next;
+                    t = t->next;
+                }
+                else if(p->val > q->val){
+                    t->next = new ListNode(q->val);
+                    q = q->next;
+                    t = t->next;
+                }
+                else{
+                    t->next = new ListNode(p->val);
+                    t = t->next;
+                    p = p->next;
+                    t->next = new ListNode(q->val);
+                    t = t->next;
+                    q = q->next;
+                }
+            }
+            if(p == NULL && q != NULL){
+                while(q != NULL)
+                {
+                    t->next = new ListNode(q->val);
+                    t = t->next;
+                    q = q->next;
+                }
+            }
+            if(p != NULL && q == NULL){
+                    while(p != NULL)
+                    {
+                        t->next = new ListNode(p->val);
+                        t = t->next;
+                        p = p->next;
+                    }
+            }
+            t->next = NULL;
+            return s;
+        }
+    }
+};
