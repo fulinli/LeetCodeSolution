1bb911877cbd3d4f4e368c60192612116ca9f898
Flynn <812264301@qq.com>
2018-12-05 23:17:07 +0800

    update

--- 143.ReorderList.cpp
+++ 143.ReorderList.cpp
@@ -26,4 +26,3 @@ public:
         reorderList(nhead);
     }
 };
-
--- 17.LetterCombinationsofaPhoneNumber.cpp
+++ 17.LetterCombinationsofaPhoneNumber.cpp
@@ -1,4 +1,4 @@
-//没想出来，直接AC别人的答案
+//没想出来，直接AC别人的答案, beats 5.645
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
@@ -24,5 +24,44 @@ public:
             letterCombination(digits, res, index + 1, t, base);
             t.pop_back();
         }
+    }
+};
+
+//beats 99% solutions
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty())
+            return {};
+        std::map<char, std::string> sm = {
+            {'2', "abc"},
+            {'3', "def"},
+            {'4', "ghi"},
+            {'5', "jkl"},
+            {'6', "mno"},
+            {'7', "pqrs"},
+            {'8', "tuv"},
+            {'9', "wxyz"},
+        };
+
+        std::vector<std::string> sf;
+        for (size_t i = 0; i < sm[digits[0]].size(); ++i) {
+            std::string s;
+            s += sm[digits[0]][i];
+            sf.push_back(s);
+        }
+        std::vector<std::string> sn = letterCombinations(digits.substr(1));
+        if(sn.empty())
+            return sf;
+        std::vector<std::string> sr;
+        for (auto & f : sf) {
+            for (auto & n : sn) {
+                std::string s = "";
+                s = s + f + n;
+                sr.push_back(s);
+            }
+
+        }
+        return sr;
     }
 };
